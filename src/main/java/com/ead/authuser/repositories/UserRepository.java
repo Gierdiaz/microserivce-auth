fa30ff83651f8a2ab6b8d15f360a647285caaf79
Gierdiaz <gierdiaz@hotmail.com>
2024-10-07 06:43:27 -0300

    Creating the Service and Service Implement

---
+++ src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ead.authuser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+import com.ead.authuser.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    
+}
