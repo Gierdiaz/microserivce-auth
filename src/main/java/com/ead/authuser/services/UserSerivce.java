99231d0d1e6d0ab87dae93b582e9150fc3d78d5a
Gierdiaz <gierdiaz@hotmail.com>
2024-10-07 17:50:49 -0300

    Creation of Get, GetAll and Delete methods in both Controller and Services

--- src/main/java/com/ead/authuser/services/UserSerivce.java
+++ src/main/java/com/ead/authuser/services/UserSerivce.java
@@ -1,5 +1,19 @@
 package com.ead.authuser.services;
 
+import com.ead.authuser.models.User;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
+
 public interface UserSerivce {
-    
+
+    List<User> findAll();
+
+    Optional<User> FindById(UUID userId);
+
+    void delete(User user);
+
 }
+
